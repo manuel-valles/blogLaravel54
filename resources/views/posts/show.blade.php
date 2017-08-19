41210a75b5fb4bd4845be06dd9fd62515f7fc1b2
manukempo <manukempo@gmail.com>
2017-08-19 20:44:06 +0100

    Form for comments with server-side validation

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,8 +14,24 @@
 					</strong> 
 					{{ $comment->body }}
 				</li>
-			@endforeach
-			</ul>
+	@endforeach
+	</ul>
+</div>
+		<hr>
+		{{-- Add a comment --}}
+		<div class="card">
+			<div class="card-block">
+				<form method="POST" action="/posts/{{ $post->id }}/comments">
+					{{ csrf_field() }}
+					<div class="form-group">
+						<textarea class="form-control" name="body" placeholder="Add your comment here." required></textarea>
+					</div>
+					<div class="form-group">
+						<button type="submit" class="btn btn-primary">Add Comment</button>
+					</div>
+				</form>
+				@include('layouts.errors')
+			</div>
 		</div>
 	</div>
 @endsection
